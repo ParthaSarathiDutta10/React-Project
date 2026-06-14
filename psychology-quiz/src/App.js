76ae7ec8d3a3449883f9fde24a6ef4087d98ab33
Partha Sarathi Dutta <coder.partha08@gmail.com>
2026-06-15 00:12:07 +0600

    initial upload

--- psychology-quiz/src/App.js
+++ psychology-quiz/src/App.js
@@ -1,73 +1,70 @@
 import { useState } from "react";
-import { questions } from "./data/questions";
-
+import {questions} from './data/questions';
 
 export default function App(){
   const [step , setStep] = useState("start");
-  const [current, setCurrent] = useState(0);
-  const [scores, setScores] = useState({});
-  const  currentQuestion = questions[current];
+  const [current , setCurrent] = useState(0);
+  const currentQuestion = questions[current];
 
-  function handleNextQuestion(type){
-    setScores((prev) => ({
-      ...prev , 
-      [type]:(prev[type] || 0)+1,
+
+  const [scores,setScores] =useState(0);
+  const result = findResult(scores);
+  function handleCurrent(type){
+       setScores((prev) => ({
+          ...prev , 
+          [type]:(prev[type] || 0)+1,
     }));
 
-    if(current < questions.length -  1){
-      setCurrent(current+1);
+
+    if(current < questions.length-1){
+
+      {setCurrent(current+1)}
     }
     else{
       setStep("result");
     }
   }
 
-  const result = getDominantType(scores);
+  function findResult(scores){
+    let dominantype = '';
+    let maxScore =-1;
+    Object.entries(scores).forEach(([type, count]) =>{
+          if(maxScore < count) {
+            maxScore= count;
+            dominantype=type;
+        }
+    })
 
-  function getDominantType(scores){
-    let maxScore = -1 ;
-    let dominantType = "";
-    Object.entries(scores).forEach(([type, count]) => {
-      if(count > maxScore){
-        maxScore =count;
-        dominantType = type;
-      }
-    }) ;
-
-    return dominantType;
-
+    return dominantype;
   }
-    return(
+
+  return(
     <div>
-      {step === 'start' && (
-        <div>
-          <h1>Start Screen</h1>
-          <button onClick={() => setStep("quiz")} >Start Quiz</button>
-        </div>
-      )}
-
-      {step === "quiz" && (
-        <div>
-          <h3>{currentQuestion.scenario}</h3>
-          
-          {currentQuestion.options.map((option, index)=>(
-            <button className="block" onClick={() => handleNextQuestion(option.type)} key={index}> {option.text}</button>
-            
-          ))}
-
-        </div>
-      )}
-
-      {step === "result" && (
-        <div>
-          <h1>Your Personality Type</h1>
-          <h2>{result}</h2>
-
-          <button onClick={() => setStep("start")} >Restart Quiz</button>
-        </div>
-      )}
+        {step === 'start' && (
+          <div>
+              <h1> Start Screen  </h1>
+              <button onClick={()=> setStep("quiz") } > Start quiz </button>
+          </div>
+        )}
+        {step === "quiz"&&(
+          <div>
+              <h1>Question:{currentQuestion.scenario}?</h1>
+              {currentQuestion.options.map((option , index ) => (
+                 <button key={index} className="block" onClick={() => handleCurrent(option.type)} > {option.text} </button>
+                  )
+                )
+              }
+          </div>
+        )}
+        {step === 'result' && (
+          <div>
+            <h1>{result}</h1>
+            <button onClick={() => setStep('start')} >Retake Quiz</button>
+          </div>
+        )}
     </div>
   );
+
+
+
 }
-
-
